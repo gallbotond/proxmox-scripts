01e1bdd35bbca070a6a608318aaca048cab24b12
botondg <botond.gall@sonrisa.hu>
2025-01-22 21:07:34 +0200

    rm spaces

--- install/amazon-corretto-21-jdk/ubuntu-server-24.sh
+++ install/amazon-corretto-21-jdk/ubuntu-server-24.sh
@@ -1,8 +1,6 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y ca-certificates apt-transport-https gnupg wget
-
 wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
 echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
-
 sudo apt-get update
 sudo apt-get install -y java-21-amazon-corretto-jdk libxi6 libxtst6 libxrender1

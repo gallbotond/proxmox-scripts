f536d9161ca5a24123fbc8dff88fcbaa8b90a5d6
botondg <botond.gall@sonrisa.hu>
2025-02-25 00:33:44 +0200

    parted script

--- allocate-all-disk/debian-server-12.sh
+++ allocate-all-disk/debian-server-12.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-sudo apt install parted -y
-sudo parted /dev/sda
+
+# Ensure the script is run as root
+# if [[ $EUID -ne 0 ]]; then
+#    echo "This script must be run as root" 
+#    exit 1
+# fi
+
+# Resize partitions
+sudo parted /dev/sda resizepart 2 -1s
+sudo parted /dev/sda resizepart 5 -1s
+
+# Update the physical volume
 sudo pvresize /dev/sda5
+
+# Resize the logical volume to use all free space
 sudo lvresize -r -l +100%FREE /dev/mapper/debian12--vg-root
